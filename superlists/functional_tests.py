6a736681947517a86df7b55a48d0bd8eee63cea8
johnhu <huzichunjohn@126.com>
2017-02-23 12:43:59 +0800

    use unittest.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,11 +1,23 @@
 from selenium import webdriver
+import unittest
 
-# no proxy
-profile = webdriver.FirefoxProfile()
-profile.set_preference('network.proxy.type', 0)
-profile.update_preferences()
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox(firefox_profile=profile)
-browser.get('http://127.0.0.1:8000')
+    def setUp(self):
+        # no proxy
+        self.profile = webdriver.FirefoxProfile()
+        self.profile.set_preference('network.proxy.type', 0)
+        self.profile.update_preferences()
 
-assert 'Django' in browser.title
+        self.browser = webdriver.Firefox(firefox_profile=self.profile)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
